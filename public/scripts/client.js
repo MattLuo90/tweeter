17e604a3e32ac401efa8cd55e5ecd68a0a9ef744
Matt <mattluo604@hotmail.com>
2021-05-19 21:05:01 +0000

    add function to render tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+$(document).ready(function(){
+  $('#tweet-text').on("submit", function (event) {
+    event.preventDefault();
+    console.log(this)
+  })
+  const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (const key of tweets) {
+      const $dataToBeAppend =createTweetElement(key);
+      $("#display").append($dataToBeAppend);
+    }
+  };
+  const createTweetElement = function(tweet) {
+    let time=timeago.format(tweet['created_at'])
+    const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}"> 
+          <span>${tweet.user.name}</span>
+        </div>
+        <div class="tag">${tweet.user.handle}</div>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <span>${time}</span>
+        <div class="icon">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-share"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+  return $tweet;
+  }
+  renderTweets(data);
+})
+
