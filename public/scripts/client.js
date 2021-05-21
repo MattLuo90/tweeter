ce1ab9dbad200099a68f1392053336f069176fed
Matt <mattluo604@hotmail.com>
2021-05-21 00:36:53 +0000

    check the codestyle with eslint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,33 +3,34 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
+$(document).ready(function() {
   $("section").hide();
-  $("form").on("submit", function (event) {
+  $("form").on("submit", function(event) {
     event.preventDefault();
     let formData = $(this).serialize();
     const dataToCheck = formData.slice(5);
-    // $("<div>").text(formData);
-    console.log(dataToCheck.length)
+    // conditions to check if user send string are empty or too long 
     if (dataToCheck.length === 0 || dataToCheck.length > 140 ){
-       $('.error-msg')
-       .text("your tweet cannot be empty or more than 140 charactors")
-       .slideDown(1000)
-       .delay(2000)
-       .fadeOut(1)
+      $('.error-msg')
+      .text("your tweet cannot be empty or more than 140 charactors")
+      .slideDown(1000)
+      .delay(2000)
+      .fadeOut(1);
+    // conditions to check if user send meaningful string
     } else if (dataToCheck === "null" || $.trim($("textarea").val()) === "") {
       $('.error-msg')
       .text("please put someting meaningful")
       .slideDown(1000)
       .delay(2000)
-      .fadeOut(1)
-    } else{
+      .fadeOut(1);
+    } else {
       $.ajax({
         url: "/tweets",
         method: "POST",
         data: formData,
       })
         .then(() => {
+          // refresh textarea to empty and reset counter number to 140 after post tweet
           $("textarea").val("");
           $('.counter').text('140');
           loadTweets();
@@ -38,48 +39,49 @@ $(document).ready(function () {
           console.log("ajax error caught");
           console.log(err);
         });
-    } 
+    }
   });
-
-  const loadTweets = function (all) {
+// function to perform a Get request from /tweets page by using ajax
+  const loadTweets = function() {
     $.ajax({
       url: "/tweets",
       method: "GET",
     })
       .then((result) => {
+        // empty database make sure there is no repeated tweets sent
         $('#display').empty();
-        renderTweets(result);  
+        renderTweets(result);
       })
       .catch((err) => {
         console.log("ajax error caught");
         console.log(err);
       });
   };
-
-  const renderTweets = function (tweets) {
+// function to render all tweets from database
+  const renderTweets = function(tweets) {
     tweets.reverse();
     for (const key of tweets) {
       const $dataToBeAppend = createTweetElement(key);
       $("#display").append($dataToBeAppend);
     }
   };
-  const escape = function (str) {
+// function to solve cross site scripting problem
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-  $( ".new-text" ).click(function() {
-    $( "section" ).slideToggle()})
-
+// function to perform a slidedown animation when press post new button and always scroll to top when press
   $(".new-text").click(function() {
-      $('html, body').animate({scrollTop:0}, '300');
-    });
-
-  const createTweetElement = function (tweet) {
+    $("section").slideToggle()
+  });
+  
+  $(".new-text").click(function() {
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+// function to creat a template of new tweet in html style
+  const createTweetElement = function(tweet) {
     let time = timeago.format(tweet["created_at"], "en_US");
-    console.log("time:", tweet["created_at"]);
-    console.log(time);
     const $tweet = $(`
     <article class="tweet">
       <header>
@@ -89,7 +91,7 @@ $(document).ready(function () {
         </div>
         <div class="tag">${tweet.user.handle}</div>
       </header>
-      <p class="tweet-content">${escape(tweet.content.text)}</p>
+      <p class="tweet-content">${escape(tweet.content.text)}</p> 
       <footer>
         <div id=“tweeted-since” style="font-size:16px">${time}</div>
         <div class="icon">
@@ -102,6 +104,6 @@ $(document).ready(function () {
   `);
     return $tweet;
   };
+  // load function to ensure the exsiting tweets rendered
   loadTweets();
-
 });
