7275398c27f0ec73ec8cc6909062c5af749cdc34
Matt <mattluo604@hotmail.com>
2021-05-21 00:37:06 +0000

    check the codestyle with eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function() {
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function() {
     let maxChar = 140;
-    remainChar= maxChar - this.value.length;
+    let remainChar = maxChar - this.value.length;
     if (remainChar < 0) {
-      $(this).parents().find('.counter').text(remainChar).css('color', 'red')
+      $(this).parents().find('.counter').text(remainChar).css('color', 'red');
     } else {
-      $(this).parents().find('.counter').text(remainChar).css('color', '#545149')
+      $(this).parents().find('.counter').text(remainChar).css('color', '#545149');
     }
-  }) 
-})
+  });
+});
 
 
 
